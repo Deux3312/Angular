a38281be2be053e13b1f10285df2353be8f43b58
Brayan Allan Medina <brayan.medina@grupolaeisz.com>
2024-10-08 17:50:30 -0600

    nueva app
    Nueva aplicacion project 2

--- Coursera/04-APIRestFul_Nodejs/project-2/app.js
+++ Coursera/04-APIRestFul_Nodejs/project-2/app.js
@@ -7,108 +7,12 @@ const newUser = new User({
     email: 'helen.bonilla@grupolaeisz.com',
     password: 'password123'
 })
-newUser.save()
-    .then(() => {
-        console.log('User inserted successfully')
-    })
-    .catch((error) => {
-        console.log('Error inserting user: ', error)
-    })
 
 
-// User.find({ email: 'brayan.medina@grupolaeisz.com' })
-//     .then((users) => {
-//         console.log('Users found: ', users)
-//     })
-//     .catch((error) => {
-//         console.log('Error finding user: ', error)
-//     })
-// dbConnect();
-// module.exports = app;
+app.length('/', (req, resp) => {
+    resp.send('Hello World');
+})
 
-
-User.find()
-    .then((users) => {
-        console.log('Users found: ', users)
-    })
-    .catch((error) => {
-        console.log('Error finding user: ', error)
-    })
-dbConnect();
-module.exports = app;
-
-//updating User
-// User.updateOne({ email: 'brayan.medina@grupolaeisz.com' }, { password: 'password3312' })
-//     .then(() => {
-//         console.log('Users updated successfully ')
-//     })
-//     .catch((error) => {
-//         console.error('Error updated user: ', error)
-//     })
-
-// Update Many users
-User.updateMany({ email: 'brayan.medina@grupolaeisz.com' }, { $set: { password: 'NEWpassword3312' } })
-    .then((result) => {
-        console.log('Document updated successfully:  ', result)
-    })
-    .catch((error) => {
-        console.error('Error updated user: ', error)
-    })
-
-//delete One
-// User.deleteOne({ email: 'helen.bonilla@grupolaeisz.com' })
-//     .then(() => {
-//         console.log('User deleted successfully')
-//     })
-//     .catch((error) => {
-//         console.error('Error deleting user: ', error)
-//     })
-
-//delete Many
-User.deleteMany({ isActive: false })
-    .then((result) => {
-        console.log('User deleted successfully', result)
-    })
-    .catch((error) => {
-        console.error('Error deleting user: ', error)
-    })
-
-
-
-User.collection.createIndex({ email: 5 })
-User.find({ email: 'brayan.medina@grupolaeisz.com' }).select({ email: 5, _id: 0 })
-    .then((users) => {
-        console.log('Users found: ', users)
-    })
-    .catch((error) => {
-        console.log('Error finding user: ', error)
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const port = 3000;
-// app.use(bodyParser.json());
-// let todos = [
-//     { id: 1, name: 'Todo 1' },
-//     { id: 2, name: 'Todo 2' },
-//     { id: 3, name: 'Todo 3' },
-//     { id: 4, name: 'Todo 4' },
-//     { id: 5, name: 'Todo 5' },
-// ];
-// app.get('/todos', (req, res) => {
-//     res.json(todos);
-// })
-// app.listen(port, () => {
-//     console.log(`ejecutandose en el puerto ${port}`)
-// });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
--- Coursera/04-APIRestFul_Nodejs/project-2/index.js
+++ Coursera/04-APIRestFul_Nodejs/project-2/index.js
@@ -1,34 +1,18 @@
-// prettier-ignore-start
-const http = require('http');
-const app = require('./project-1/app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const dbConnect = require('./db/dbConnect');
+const User = require('./db/userModel');
+const newUser = new User({
+    email: 'helen.bonilla@grupolaeisz.com',
+    password: 'password123'
+})
 
-app.set('port', process.env.PORT || 3000); // Establece un puerto por defecto si no hay variable de entorno
-const server = http.createServer(app);
 
-// Establecer el número máximo de conexiones
-server.maxConnections = 100; // Por ejemplo, establece el límite en 100
+app.get('/', (req, resp) => {
+    resp.send('Hello World');
+})
 
-// Escuchar en el puerto especificado
-server.listen(app.get('port'), () => {
-    console.log('Server is listening on port ' + app.get('port'));
-});
-
-// prettier-ignore-end
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const app = express();
-// const port = 3000;
-// app.use(bodyParser.json());
-// let todos = [
-//     { id: 1, name: 'Todo 1' },
-//     { id: 2, name: 'Todo 2' },
-//     { id: 3, name: 'Todo 3' },
-//     { id: 4, name: 'Todo 4' },
-//     { id: 5, name: 'Todo 5' },
-// ];
-// app.get('/todos', (req, res) => {
-//     res.json(todos);
-// })
-// app.listen(port, () => {
-//     console.log(`ejecutandose en el puerto ${port}`)
-// });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
