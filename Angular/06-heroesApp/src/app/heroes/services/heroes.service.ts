3a2a4c85bc19e29a4452c822782a67979c037cfd
Deux777 <brayamedina3312@gmail.com>
2024-11-18 21:02:35 -0600

    hero 06
    Hero 06 con método de llamada

--- Angular/06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ Angular/06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -10,5 +10,8 @@ export class HeroesService {
     constructor(private http:HttpClient) { }
     getHeroes():Observable<Hero[]>{
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+    }
+    getHeroById(id:string):Observable<Hero|undefined>{
+        return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(catchError(error => of(undefined)));
     }
 }
