f92643d4f7d5cceb315eac06751b60d4822f8467
Deux777 <brayamedina3312@gmail.com>
2024-11-20 21:04:52 -0600

    funcion
    Funcional para correr backend npm run backend

--- Angular/06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ Angular/06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { delay, switchMap } from 'rxjs';
 import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
@@ -19,6 +19,7 @@ export class HeroPageComponent implements OnInit{
   }
   ngOnInit(): void {
     this.activatedRoute.params.pipe(
+      delay(3000),
       switchMap(({id})=>this.heroService.getHeroById(id)),
     ).subscribe(hero=> {
       if(!hero) return this.router.navigate(['/heroes/list'])
@@ -26,5 +27,8 @@ export class HeroPageComponent implements OnInit{
       console.log({hero})
       return
     });
+  }
+  goBack():void{
+    this.router.navigateByUrl('heroes/list')
   }
 }
