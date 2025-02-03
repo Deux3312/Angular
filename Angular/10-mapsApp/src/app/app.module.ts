dc28f6638309603b6ac838d57d374929557a866e
Brayan Allan Medina <brayan.medina@grupolaeisz.com>
2025-02-03 17:54:35 -0600

    10 angular
    10 angular mas semana 3

--- Angular/10-mapsApp/src/app/app.module.ts
+++ Angular/10-mapsApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapsModule } from './maps/maps/maps.module';
+import { MapsModule } from './maps/maps.module';
 
 @NgModule({
   declarations: [
