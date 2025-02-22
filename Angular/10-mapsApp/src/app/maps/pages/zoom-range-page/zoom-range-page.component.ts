f9b21639a4a50d4769df1c0bd1c81820134daa3a
Brayan Allan Medina <brayan.medina@grupolaeisz.com>
2025-02-22 11:23:00 -0600

    maps
    Mapas 10

--- Angular/10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ Angular/10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent  implements AfterViewInit{
+    
+    constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+ @ViewChild('map') divMap?: ElementRef;
 
+
+
+  ngAfterViewInit(): void {
+    if(!this.divMap) throw 'Elemento no encontrado';
+    if (isPlatformBrowser(this.platformId)) {
+      const map = new maplibregl.Map({
+        container: this.divMap.nativeElement, // container id
+        style: 'https://demotiles.maplibre.org/style.json', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+      });
+    }
+  }
 }
