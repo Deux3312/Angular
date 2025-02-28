def435baa8cb8faf56954397c8ace367c31feeff
Brayan Allan Medina <brayan.medina@grupolaeisz.com>
2025-02-28 08:31:57 -0600

    .
    .

--- Angular/10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ Angular/10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import maplibregl,{LngLat, Map} from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
-
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+  
+  @ViewChild('map') divMap?: ElementRef;
+ 
+ public zoom:number = 0;
+ public map?: Map;
+ public currentLngLat: LngLat = new LngLat(-74.5, 40);
+ 
+   ngAfterViewInit(): void {
+     if(!this.divMap) throw 'Elemento no encontrado';
+     if (isPlatformBrowser(this.platformId)) {
+       this.map = new maplibregl.Map({
+         container: this.divMap.nativeElement, // container id
+         style: 'https://demotiles.maplibre.org/style.json', // style URL
+         center: this.currentLngLat,
+         zoom: this.zoom // starting zoom
+       });
+ 
+     }
+ 
+     
+   }
 }
